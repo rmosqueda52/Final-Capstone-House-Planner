8d3365ed90c58c86f713e28a17458069ce9a6c4d
Rafael Mosqueda <rmosqueda306@gmail.com>
2022-05-31 10:41:57 -0400

    Roles assigned on Registartion

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -32,6 +32,27 @@
         v-model="user.confirmPassword"
         required
       />
+      <input
+        type="radio"
+        id="home-buyer"
+        class="form-control"
+        v-model="user.role"
+        name="Role"
+        value="Home_Buyer"
+        required
+      />
+      <label for="role" id="home-buyer">Home Buyer</label>
+      <input
+        type="radio"
+        id="system-admin"
+        class="form-control"
+        v-model="user.role"
+        name="Role"
+        value="System_Admin"
+        required
+      />
+      <label for="role" id="system-admin">System Administrator</label>
+
       <router-link :to="{ name: 'login' }">Have an account?</router-link>
       <button class="btn btn-lg btn-primary btn-block" type="submit">
         Create Account
@@ -41,35 +62,35 @@
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -77,14 +98,14 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
