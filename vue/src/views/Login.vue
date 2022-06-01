a6eb70b2599e7a4ed8311d33179ca0da2b9e9167
Christopher Espinosa <work.chrisespi@gmail.com>
2022-06-01 11:30:46 -0500

    Chris

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,8 +1,8 @@
 <template>
-<body id="login-body">
+<body id="login-body" style="margin: -1%;">
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Please Sign in to your Account</h1>
       <div
         class="alert alert-danger"
         role="alert"
@@ -32,12 +32,23 @@
         v-model="user.password"
         required
       />
-      <button class="sign-in" type="submit">Sign in</button>
+      <button class="learn-more">
+  <span class="circle" aria-hidden="true">
+  <span class="icon arrow"></span>
+  </span>
+  <span class="button-text">Sign In</span>
+</button>
     </form>
   </div>
   <div id="new-account-container">
-    <h3 id="create-account">Create an Account?</h3>
-    <router-link id="new-account" :to="{ name: 'register' }">Sign up</router-link>
+    <h3 id="create-account">Are you new here?</h3>
+    <router-link id="new-account" :to="{ name: 'register' }"><button class="cta">
+  <span class="hover-underline-animation"> <b>Create Account</b> </span>
+  <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
+    <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
+  </svg>
+</button></router-link>
+    
   </div>
   
 </body>
@@ -88,25 +99,29 @@ export default {
   align-items: center;
   flex-direction: row;
   justify-content: center;
-  background-image: url(https://i.imgur.com/O9EDJOt.jpeg);
-  background-size: cover;
+  background-image: url(https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80);
+  background-size: contain;
   /* background-color: #023047; */
 
 }
 #new-account{
    font-family: 'Montserrat', sans-serif;
    size: 50px;
-   color: whitesmoke;
+   color: rgb(255, 255, 255);
    margin-top: 50px;
 }
 #new-account-container {
-  background-color: #219ebc;
+  background-image: url(https://i.pinimg.com/originals/83/30/8f/83308f3e6c6fde56dd45f162c718ab60.jpg);
   /* padding: 150px; */
-  padding-top:175px;
-  padding-bottom: 750px;
+  padding-top:431px;
+  padding-bottom: 500px;
   padding-left: 150px;
   padding-right: 150px;
-  margin-left: 5%;
+  margin-left: 27%;
+  width: 300px;
+  text-align: center;
+  
+ 
   /* align-content: baseline; */
   /* flex-grow: .4; */
   /* text-align: center; */
@@ -116,6 +131,8 @@ export default {
 #create-account{
   font-family: 'Montserrat', sans-serif;
   color: rgb(0, 0, 0);
+  font-size: 20px;
+  
 }
 #login{
   display: flex;
@@ -126,6 +143,7 @@ export default {
   size: 50px;
   text-align: center;
   font-family: 'Montserrat', sans-serif;
+  color: rgb(0, 0, 0);
 }
 h1{
   text-align: center;
@@ -135,31 +153,195 @@ h1{
   flex-direction: column;
   margin-bottom: 10px;
   font-family: 'Montserrat', sans-serif;
-  height: 25px;
-  border-radius: 10px;
+  height: 40px;
+  width: 401px;
+  border-radius: 30px;
+  text-align: center;
+  font-size: 20px;
+  border: 0px;
 }
 #password{
   display: flex;
   flex-direction: column;
   margin-bottom: 10px;
   font-family: 'Montserrat', sans-serif;
-  height: 25px;
-  border-radius: 10px;
+  height: 40px;
+  width: 401px;
+  border-radius: 30px;
+  text-align: center;
+  font-size: 20px;
+  border: 0px;
 }
 .sign-in{
   display: flex;
   flex-direction: column;
   margin-top: 25px;
   border-radius: 10px;
+  margin-left: 176px;
+  padding: 10px;
+  line-height: 80px;
+  box-sizing: border-box;
+  padding-left: 50px;
+  padding-right: 50px;
 }
 .form-signin{
-  background-color: #ffb703 1;
+  background-color: rgba(255, 255, 255, 0.301);
   padding: 50px;
-  padding-bottom: 500px;
-  backdrop-filter: blur(15px);
+  padding-bottom: 200px;
+  backdrop-filter: blur(10px);
   border: 0px solid black;
   border-radius: 5px;
   position: relative;
-
+  margin-left: 97%;
 }
+
+html {
+  overflow-y: hidden; 
+  overflow-x: hidden;
+  margin: 0%;
+}
+/* From uiverse.io */
+button {
+ position: relative;
+ display: inline-block;
+ cursor: pointer;
+ outline: none;
+ border: 0;
+ vertical-align: middle;
+ text-decoration: none;
+ background: transparent;
+ padding: 0;
+ font-size: inherit;
+ font-family: inherit;
+}
+
+button.learn-more {
+ width: 12rem;
+ height: auto;
+}
+
+button.learn-more .circle {
+ transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+ position: relative;
+ display: block;
+ margin: 0;
+ width: 3rem;
+ height: 3rem;
+ background: #000000;
+ border-radius: 1.625rem;
+}
+
+button.learn-more .circle .icon {
+ transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+ position: absolute;
+ top: 0;
+ bottom: 0;
+ margin: auto;
+ background: #fff;
+}
+
+button.learn-more .circle .icon.arrow {
+ transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+ left: 0.625rem;
+ width: 1.125rem;
+ height: 0.125rem;
+ background: none;
+}
+
+button.learn-more .circle .icon.arrow::before {
+ position: absolute;
+ content: "";
+ top: -0.29rem;
+ right: 0.0625rem;
+ width: 0.625rem;
+ height: 0.625rem;
+ border-top: 0.125rem solid #fff;
+ border-right: 0.125rem solid #fff;
+ transform: rotate(45deg);
+}
+
+button.learn-more .button-text {
+ transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+ position: absolute;
+ top: 0;
+ left: 0;
+ right: 0;
+ bottom: 0;
+ padding: 0.75rem 0;
+ margin: 0 0 0 1.85rem;
+ color: #000000;
+ font-weight: 700;
+ line-height: 1.6;
+ text-align: center;
+ text-transform: uppercase;
+}
+
+button:hover .circle {
+ width: 100%;
+}
+
+button:hover .circle .icon.arrow {
+ background: #fff;
+ transform: translate(1rem, 0);
+}
+
+button:hover .button-text {
+ color: #fff;
+}
+/* From uiverse.io by @alexmaracinaru */
+.cta {
+ border: none;
+ background: none;
+ padding-left: 3px;
+ 
+}
+
+.cta span {
+ padding-bottom: 7px;
+ letter-spacing: 4px;
+ font-size: 15px;
+ 
+ padding-right: 15px;
+ text-transform: uppercase;
+ 
+ 
+}
+
+.cta svg {
+ transform: translateX(-8px);
+ transition: all 0.3s ease;
+}
+
+.cta:hover svg {
+ transform: translateX(0);
+}
+
+.cta:active svg {
+ transform: scale(0.9);
+}
+
+.hover-underline-animation {
+ position: relative;
+ color: black;
+ padding-bottom: 20px;
+}
+
+.hover-underline-animation:after {
+ content: "";
+ position: absolute;
+ width: 100%;
+ transform: scaleX(0);
+ height: 1.5px;
+ bottom: 0;
+ left: 0;
+ background-color: #000000;
+ transform-origin: bottom right;
+ transition: transform 0.25s ease-out;
+}
+
+.cta:hover .hover-underline-animation:after {
+ transform: scaleX(1);
+ transform-origin: bottom left;
+}
+
 </style>
