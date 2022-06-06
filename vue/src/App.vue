fb4d33e47d4b98d15d51aabc87f30ca41282bdd5
Rafael Mosqueda <rmosqueda306@gmail.com>
2022-06-06 09:36:09 -0400

    Fixed double home buttons

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> -->
     </div>
     <router-view />
   </div>
 </template>
 <style>
-#nav{
+html {
+  padding: 10px;
+}
+#nav {
   position: absolute;
   margin-top: 15px;
   margin-left: 600px;
-  
 }
 </style>
